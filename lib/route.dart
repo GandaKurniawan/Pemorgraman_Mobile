edb7f4ac91ca1595b122c3e63cd25ed0d5124e30
GandaKurniawan <gandakur09@gmail.com>
2020-10-28 23:20:31 +0700

    gand

--- lib/route.dart
+++
@@ -1,9 +0,0 @@
-// import 'dart:js';
-import 'package:combe_app/screens/sign_in/sign_in.dart';
-import 'package:combe_app/screens/view/view_screen.dart';
-import 'package:flutter/material.dart';
-
-final Map<String, WidgetBuilder> routes = {
-  View_Screen.routeName: (context) => View_Screen(),
-  SignIn_Screen.routeName: (context) => SignIn_Screen(),
-};
